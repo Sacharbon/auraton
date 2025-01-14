e517a4ee5e78fa1f457196ef7f1d5bdb755ce966
Aymeric <aymeric.jouannet-mimy@epitech.eu>
2025-01-14 11:37:57 +0100

    feat(components/attribut.tsx): add attribut component

--- front/src/components/dashboard/attribut.tsx
+++ front/src/components/dashboard/attribut.tsx
@@ -4,5 +4,9 @@ interface AttributProps {
 }
 
 export const Attribut = ({attribut, color}:AttributProps) => {
-    
+    return (
+        <div className={`border-black rounded-lg pl-2 pr-2 bg-opacity-50 ${color}`}>
+            <div className={`flex items-center font-poppins text-black`}>{attribut}</div>
+        </div>
+    );
 }
