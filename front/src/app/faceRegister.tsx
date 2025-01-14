5cea4c749e60e782866a3d8c4a39ba456a6c2a64
Sacharbon <sacha.dujardin@epitech.eu>
2025-01-14 13:13:59 +0100

    feat(register): register someone with face

--- front/src/app/faceRegister.tsx
+++ front/src/app/faceRegister.tsx
@@ -1,6 +1,6 @@
 import * as faceapi from 'face-api.js';
 
-export const registerUser = async (videoElement: HTMLVideoElement): Promise<string | null> => {
+export const registerUser = async (videoElement: HTMLVideoElement, firstName: string, lastName: string, role: string | null, pictureUrl: string | null): Promise<object | null> => {
   try {
     const MODEL_URL = '/models';
     await Promise.all([
@@ -15,7 +15,23 @@ export const registerUser = async (videoElement: HTMLVideoElement): Promise<stri
       .withFaceDescriptor();
 
     if (detection) {
-      console.log("registration");
+      const data = {
+        firstName: firstName,
+        lastName: lastName,
+        faceDescriptor: [Object.values(detection.descriptor)],
+        roles: [role],
+        pictureUrl: pictureUrl,
+      };
+      const response = await fetch("http://localhost:3000/users", {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+      })
+      const new_user = await response.json();
+      console.log(new_user);
+      return new_user;
     } else {
       return null;
     }
