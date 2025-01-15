a7619fff5bd555cd05919bf99254297e4f929955
Sacharbon <sacha.dujardin@epitech.eu>
2025-01-15 11:26:48 +0100

    feat(utils): ranking

---
+++ front/src/utils/ranking.tsx
@@ -0,0 +1,9 @@
+import { getUsers } from './getUsers.tsx'
+
+export async function ranking() {
+  const users = await getUsers();
+  users.sort((a, b) => b.aura - a.aura);
+  return users;
+}
+
+await ranking();
