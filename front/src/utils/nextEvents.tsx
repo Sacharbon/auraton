55de560fc2a7c4c4f8d2847b09eb7cebbde6ba50
Sacharbon <sacha.dujardin@epitech.eu>
2025-01-15 22:48:47 +0100

    feat(utils): nextEvents.tsx

--- front/src/utils/nextEvents.tsx
+++ front/src/utils/nextEvents.tsx
@@ -3,12 +3,6 @@ import { getEvents } from './getEvents.tsx'
 export async function nextEvents() {
   const events = await getEvents();
 
-  //for developement only
-  let dt = Date.now();
-  events.forEach(element => {
-    element.scheduledAt = dt += 40; 
-  });
-
-  events.sort((a: any, b: any) => b.scheduledAt - a.scheduledAt);
+  events.sort((a: any, b: any) => new Date(b.scheduledAt) - new Date(a.scheduledAt));
   return events.slice(0, 3);
 }
