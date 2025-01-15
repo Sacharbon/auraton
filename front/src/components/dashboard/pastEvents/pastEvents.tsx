f5c3ea0b71d809978301f59282da4f0f13b0f35d
Aymeric <aymeric.jouannet-mimy@epitech.eu>
2025-01-15 10:57:16 +0100

    refactor(pastEvents.tsx): grow the images

--- front/src/components/dashboard/pastEvents/pastEvents.tsx
+++ front/src/components/dashboard/pastEvents/pastEvents.tsx
@@ -8,9 +8,9 @@ interface pastEventsProps {
 
 export const PastEvents = ({image1, image2}: pastEventsProps) => {
     return (
-        <div className="flex pl-5 space-x-16 w-full h-48 ">
-            <Image className="rounded-lg object-cover hover:scale-105 duration-300 transition-all" width={300} height={100} alt="image1" src={image1}></Image>
-            <Image className="rounded-lg object-cover hover:scale-105 duration-300 transition-all" width={300} height={100} alt="image1" src={image2}></Image>
+        <div className="flex space-x-10 w-full h-48 ">
+            <Image className="rounded-lg object-cover hover:scale-105 duration-300 transition-all" width={350} height={100} alt="image1" src={image1}></Image>
+            <Image className="rounded-lg object-cover hover:scale-105 duration-300 transition-all" width={350} height={100} alt="image1" src={image2}></Image>
         </div>
     );
 }
