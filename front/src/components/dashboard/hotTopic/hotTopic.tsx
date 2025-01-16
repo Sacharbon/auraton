4a829b7708e3fa027f4bcdede8770e101d27e56b
Aymeric <aymeric.jouannet-mimy@epitech.eu>
2025-01-16 10:53:00 +0100

    feat(hotTopic.tsx): update hotTopic component

--- front/src/components/dashboard/hotTopic/hotTopic.tsx
+++ front/src/components/dashboard/hotTopic/hotTopic.tsx
@@ -1,11 +1,15 @@
 import Image from "next/image";
 import { Users } from "@/components/dashboard/users";
 import { Title } from "@/components/dashboard/title";
+import { LikeButton } from "../Buttons/likeButton";
+import { CommentButton } from "../Buttons/commentButton";
+import { Button } from "@chakra-ui/react";
 
 interface HotTopicProps {
   image?: string;
   title: string;
   content: string;
+  nbRegistered: string
   topicAttribut: string;
   topicColorAttribut: string;
   userIcon?: string;
@@ -19,6 +23,7 @@ export const HotTopic = ({
   image,
   title,
   content,
+  nbRegistered,
   topicAttribut,
   topicColorAttribut,
   userIcon,
@@ -28,7 +33,7 @@ export const HotTopic = ({
   userColorAttribut,
 }: HotTopicProps) => {
   return (
-    <div className="flex h-[90%] space-x-16 p-5 rounded-3xl shadow-3xl">
+    <div className="flex h-[90%] space-x-16 w-100 p-5 rounded-3xl shadow-3xl">
       <div className="flex space-x-5">
         {image && (
           <Image
@@ -40,7 +45,12 @@ export const HotTopic = ({
           ></Image>
         )}
       </div>
-      <div className="flex-row space-y-4">
+      <div className="flex flex-col w-[560px] space-y-4 justify-between">
+        <Title
+          title={title}
+          attribut={topicAttribut}
+          colorAttribut={topicColorAttribut}
+        ></Title>
         {userName && userIcon && userAttribut && userRank && userColorAttribut && (
           <Users
             name={userName}
@@ -50,13 +60,19 @@ export const HotTopic = ({
             colorAttribut={userColorAttribut}
           ></Users>
         )}
-        <Title
-          title={title}
-          attribut={topicAttribut}
-          colorAttribut={topicColorAttribut}
-        ></Title>
-        <div className="font-poppins text-base text-black text-wrap">
+        <div className="font-poppins text-base text-black w-[560px] h-[140px] text-wrap overflow-hidden">
           {content}
+        </div>
+        <div className="flex items-center justify-start space-x-5">
+          <div className="text-lg opacity-50">{nbRegistered} inscrits</div>
+          <Button size={"2xs"}
+                  bg={"black"}
+                  color={"white"}
+                  width={120}
+                  height={10}
+                  rounded={"2xl"}>S'inscrire</Button>
+          <CommentButton nbComments="3"></CommentButton>
+          <LikeButton nbLikes="12"></LikeButton>
         </div>
       </div>
     </div>
