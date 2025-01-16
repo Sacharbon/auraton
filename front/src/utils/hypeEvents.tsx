ccaaee0e025b845d9ebfd8fb08bb1ce277cae51e
Sacharbon <sacha.dujardin@epitech.eu>
2025-01-16 11:07:25 +0100

    feat(utils): hypeEvent

--- front/src/utils/hypeEvents.tsx
+++
@@ -1,7 +0,0 @@
-import { getEvent } from './getEvents.tsx';
-
-export async function hypeEvents() {
-  const events = getEvents();
-  events.sort((a, b) => (b.like + b.registeredUsers.length()) - (a.like + a.registeredUsers.length()))
-  return events[0];
-}
