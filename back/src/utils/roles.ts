542d181bb042dea6ab60c0603b646c4bd77cc576
nL1x <nathan.jeannot@epitech.eu>
2025-01-16 09:56:53 +0100

    fix(back/comments, back/users): fix the comments & fix the user update roles

--- back/src/utils/roles.ts
+++ back/src/utils/roles.ts
@@ -3,5 +3,5 @@ export function isValidRolesFormat(roles: any)
     if (!Array.isArray(roles))
         return false;
 
-    return roles.filter(elem => typeof elem === "string").length === 0;
+    return roles.filter(elem => typeof elem !== "string").length === 0;
 }
