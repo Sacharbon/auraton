dd0e58849a725de4d586df1cbb657ff22df3ff7b
Aymeric <aymeric.jouannet-mimy@epitech.eu>
2025-01-16 15:53:35 +0100

    feat(page.tsx): add the podium

--- front/src/app/ranking/page.tsx
+++ front/src/app/ranking/page.tsx
@@ -1,10 +1,18 @@
 import Header from "@/components/dashboard/header";
+import { Podium } from "@/components/dashboard/ranking/podium";
+import { Images } from "@/components/test";
+import Image from "next/image";
 
 export default function Ranking() {
   return (
-    <div className="w-screen h-screen flex flex-col gap-10 p-4">
+    <div className="w-screen h-screen flex flex-col gap-16 px-12 pt-12 pb-8">
       <div className="w-full h-fit">
         <Header />
+      </div>
+        <Podium image1st={Images.aurelien}
+                image2nd={Images.aurelien}
+                image3trd={Images.aurelien}></Podium>
+      <div>
       </div>
     </div>
   );
