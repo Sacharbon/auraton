0064d029bd171d6636ade532e66d02d0b88a6e66
nL1x <nathan.jeannot@epitech.eu>
2025-01-16 09:38:31 +0100

    feat(back/registrations): adds the registration to the events

--- back/src/models/registration.ts
+++ back/src/models/registration.ts
@@ -1,6 +1,38 @@
-import {Request, Response} from "express";
+import {DataTypes, Model, Sequelize} from 'sequelize';
+import User from "@models/user";
+import Event from "@models/event";
 
-export default async function registerUserToEvent(req: Request, res: Response)
-{
+class Registration extends Model {
+    declare userId: number;
+    declare eventId: number;
 
+    declare user: User;
 }
+
+export async function initRegistrationModel(database: Sequelize) {
+    Registration.init(
+        {
+            userId: {
+                type: DataTypes.INTEGER,
+                primaryKey: true,
+                references: {
+                    model: User,
+                    key: 'id'
+                }
+            },
+            eventId: {
+                type: DataTypes.INTEGER,
+                primaryKey: true,
+                references: {
+                    model: Event,
+                    key: 'id'
+                }
+            },
+        },
+        {
+            sequelize: database, modelName: 'Registration'
+        }
+    );
+}
+
+export default Registration;
